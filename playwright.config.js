dc2b577dbd6d947c53f9d22828dc9853f7fc198b
Dkrown <adewale.obasa@outlook.fr>
2025-02-14 16:51:39 +0100

    Modified playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -1,10 +1,25 @@
 // playwright.config.js
+//const { defineConfig } = require('@playwright/test');
+
+//module.exports = defineConfig({
+//  testDir: './tests',  // Or './e2e', './src/tests', or wherever your tests are
+//  reporter: 'html', // Enable the HTML reporter
+//  use: {
+//    baseURL: 'http://localhost:3000', // Replace with the base URL of your app (if applicable)
+//  },
+//});
+
+// playwright.config.js
 const { defineConfig } = require('@playwright/test');
 
 module.exports = defineConfig({
-  testDir: './tests',  // Or './e2e', './src/tests', or wherever your tests are
-  reporter: 'html', // Enable the HTML reporter
+  testDir: './tests',
+  reporter: [
+    ['list'],  // For console output
+    ['junit', { outputFile: 'jest-results/junit.xml' }],  // JUnit reporter
+    ['html', { open: 'never' }] //HTML reporter
+  ],
   use: {
-    baseURL: 'http://localhost:3000', // Replace with the base URL of your app (if applicable)
+    baseURL: 'http://localhost:3000',
   },
 });
